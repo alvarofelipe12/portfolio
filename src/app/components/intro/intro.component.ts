88134f5d4f8385a23698fedd00f4d874fade0e4b
Alvaro Felipe Garcia Mendez <alvarofelipe12@gmail.com>
2024-01-30 12:10:17 -0500

    Intro and contact added

--- src/app/components/intro/intro.component.ts
+++ src/app/components/intro/intro.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-intro',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./intro.component.scss']
 })
 export class IntroComponent {
+
+  constructor(
+    private router: Router
+  ) { }
+
+  scroll(el: string) {
+    if (document.getElementById(el)) {
+      document.getElementById(el)!.scrollIntoView({ behavior: 'smooth' });
+    } else {
+      this.router.navigate(['/home']).then(() => document.getElementById(el)!.scrollIntoView({ behavior: 'smooth' }));
+    }
+  }
 
 }
