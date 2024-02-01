5c815e415d8fc1b6ccdaef3784d4d11feee75a85
Alvaro Felipe Garcia Mendez <alvarofelipe12@gmail.com>
2024-02-01 16:17:13 -0500

    Add projects and style fixes

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-projects',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent {
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: false,
+    navSpeed: 700,
+    items: 1,
+    autoplay: true,
+    autoplayTimeout: 3000
+  }
+
+  @ViewChild('imgContainer')
+  imgContainer!: ElementRef;
+
+  debug() {
+
+    this.imgContainer.nativeElement.scroll({
+      top: this.imgContainer.nativeElement.scrollHeight,
+      left: 0,
+      behavior: 'smooth',
+    });
+  }
 
 }
